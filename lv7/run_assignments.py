fdcd2206a231676fee400a73460951dd65b25360
ivan <idolibasic@outlook.com>
2025-01-09 21:52:35 +0100

    Conclude the game module.

--- lv7/run_assignments.py
+++ lv7/run_assignments.py
@@ -2,21 +2,39 @@ from lv7.zd1.player import Player
 from lv7.zd1.game_state_display import GameState
 from lv7.zd2.hangman_points_calculator import HangmanPointsCalculator
 from lv7.zd2.length_points_calculator import LengthPointsCalculator
+from lv7.zd3.hangman_game import HangmanGame
+
 
 # How to import modules https://www.learnpython.org/en/Modules_and_Packages
 
 def run():
-    player = Player("Pero", 5)
-    game = GameState("🟢", "🔴")
-    game.display(player.name, max(player.health_points, 6), player.health_points, "gt", "table")
+    #player = Player("Pero", 5)
+    #game = GameState()
+    #game.display(player.name, max(player.health_points, 6), player.health_points, "gt", "table")
     # Max function https://www.w3schools.com/python/ref_func_max.asp
 
-    base_calc = HangmanPointsCalculator(3, 2)
-    base_pts = base_calc.calculate_points("gt", "table", player)
-    print(f"Base calculator points: {base_pts}")
+    #base_calc = HangmanPointsCalculator(3, 2)
+    #base_pts = base_calc.calculate_points("gt", "table", player)
+    #print(f"Base calculator points: {base_pts}")
 
-    advanced_calc = LengthPointsCalculator(3, 2, 2)
-    advanced_pts = advanced_calc.calculate_points("gt", "table", player)
-    print(f"Advanced calculator points: {advanced_pts}")
+    #advanced_calc = LengthPointsCalculator(3, 2, 2)
+    #advanced_pts = advanced_calc.calculate_points("gt", "table", player)
+    #print(f"Advanced calculator points: {advanced_pts}")
+
+    hangman_game = HangmanGame("Pero", "table")
+    while not hangman_game.is_over():
+        hangman_game.display()
+        game_mode = input("Enter c for letter, w for word guess:\n")
+        if game_mode.lower() == 'c':
+            guess_letter = input("Enter letter:\n")
+            hangman_game.letter_guess(guess_letter)
+        else:
+            guess_word = input("Guess word:\n")
+            hangman_game.word_guess(guess_word)
+    if hangman_game.has_won():
+        print("Congrats, you won!")
+    else:
+        print(f"You lost. The guess word was: '{hangman_game.guess_word}'.")
+
 
 run()
--- lv7/zd3/hangman_game.py
+++ lv7/zd3/hangman_game.py
@@ -6,9 +6,42 @@ class HangmanGame:
     def __init__(self, player_name: str, guess_word: str):
         self.__max_lives: int = 6
         self.__guess_word = guess_word
-        self._guess_attempt = ""
+        self.__guess_attempt = ""
         self.__player = Player(player_name, self.__max_lives)
         self.__display_tool = GameState()
         self.__points_calculator = HangmanPointsCalculator()
 
-        # A class is missing some implementation details... TBD...
+    @property
+    def guess_word(self):
+        return self.__guess_word
+
+    def display(self) -> None:
+        self.__display_tool.display(self.__player.name,
+                                    self.__max_lives,
+                                    self.__player.health_points,
+                                    self.__guess_attempt,
+                                    self.__guess_word)
+
+    def has_won(self) -> bool:
+        for i in self.__guess_word:
+            if i not in self.__guess_attempt:
+                return False
+        return True
+
+    def has_lost(self) -> bool:
+        return not self.__player.is_alive()
+
+    def is_over(self) -> bool:
+        return self.has_lost() or self.has_won()
+
+    def letter_guess(self, letter: str) -> None:
+        if letter in self.__guess_word and letter not in self.__guess_attempt:
+            self.__guess_attempt += letter
+        else:
+            self.__player.reduce_health_points()
+
+    def word_guess(self, word: str) -> None:
+        if word == self.__guess_word:
+            self.__guess_attempt = self.__guess_word
+        else:
+            self.__player.reduce_health_points()
